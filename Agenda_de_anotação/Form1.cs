d47234a377bf58be8240e18d1777103e5d6a741c
Estudos <Estudos@JUNIO>
2022-07-30 22:59:40 -0300

    Adicionar arquivos de projeto.

---
+++ Agenda_de_anotação/Form1.cs
@@ -0,0 +1,20 @@
+namespace Agenda_de_anotação
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
